6506113ea1bf3c1fb078451cbc56acf80f8328b7
Adam Sebesta <adam.sebesta5@gmail.com>
2022-06-21 21:59:22 +0200

    project divider working as intended

--- components/projects/Projects.vue
+++ components/projects/Projects.vue
@@ -6,21 +6,23 @@
         v-if="i < maxProjects"
         :to="v.to"
         :class="'project' + (i === 0 ? ' first' : '')"
+        @mouseover="hover = i"
+        @mouseleave="hover = -1"
       >
         <h4>{{ v.title }}</h4>
         <div class="project-lower">
           <div>
             <span class="text-16-medium uppercase">{{
-              v.sector + " / " + v.year
+              v.year + " // " + v.sector
             }}</span>
           </div>
           <div class="stack">
             <span class="text-16-medium uppercase">{{
-              v.stack.join(" / ")
+              v.stack.join(" // ")
             }}</span>
           </div>
         </div>
-        <ProjectsDivider color="var(--black-03)" />
+        <ProjectsDivider color="var(--black-03)" :hover="hover == i" />
       </nuxt-link>
     </template>
   </div>
@@ -30,6 +32,7 @@ import { IProjectOb } from "~~/types/projects";
 import Projects from "../../config/projects";
 const projects: IProjectOb = Projects;
 const seeMoreClicked: Boolean = false;
+const hover = ref(-1);
 const maxProjects = computed((): Number => {
   return seeMoreClicked ? 100 : 4;
 });
@@ -43,11 +46,17 @@ const maxProjects = computed((): Number => {
   background: var(--white-01);
 
   .project {
-    padding: var(--space-xxl) var(--space-xl) var(--space-sm) var(--space-xl);
-    width: calc(100%);
+    padding: 0 var(--space-xl) 0 var(--space-xl);
+    margin: var(--space-xxl) 0 var(--space-sm) 0;
+    width: 100%;
     color: black;
     position: relative;
-
+    &:hover {
+      .active {
+        transform: translateX(0%);
+        width: calc(100%);
+      }
+    }
     &.first {
       // margin-top: calc(var(--space-lg) +);
     }
--- components/projects/ProjectsDivider.vue
+++ components/projects/ProjectsDivider.vue
@@ -1,33 +1,77 @@
 <template>
-  <svg
-    width="1719"
-    height="8"
-    viewBox="0 0 1719 8"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    id="projectDivider"
-  >
-    <rect
-      width="56"
+  <div class="divider-wrapper">
+    <svg
+      width="1719"
       height="8"
-      transform="matrix(1 0 0 -1 1663 8)"
-      :fill="color"
-    />
-    <path d="M1651 8H1663V0L1651 7V8Z" :fill="color" />
-    <rect y="7" width="1719" height="1" :fill="color" />
-  </svg>
+      viewBox="0 0 1719 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="project-divider"
+    >
+      <rect
+        width="56"
+        height="8"
+        transform="matrix(1 0 0 -1 1663 8)"
+        :fill="color"
+        class="project-divider-line"
+      />
+      <path d="M1651 8H1663V0L1651 7V8Z" :fill="color" />
+      <rect y="7" width="1719" height="1" :fill="color" />
+    </svg>
+    <div class="active-wrapper">
+      <svg
+        width="1530"
+        height="8"
+        viewBox="0 0 1530 8"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="project-divider active"
+      >
+        <rect
+          width="1518"
+          height="8"
+          transform="matrix(1 0 0 -1 12 8)"
+          :fill="color"
+        />
+        <path d="M0 8H12V0L0 7V8Z" :fill="color" />
+      </svg>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 const props = defineProps({ color: String });
 </script>
 <style lang="scss">
-#projectDivider {
-  width: calc(100% + var(--space-xl));
-  transform: translateX(calc(var(--space-xl) * -1));
-  path {
-    transform: translateX(1px);
+.divider-wrapper {
+  height: 8px;
+  position: relative;
+  .active-wrapper {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+    height: 8px;
   }
-}
-.projectDividerline {
+  .project-divider {
+    position: absolute;
+    direction: rtl;
+    top: 0;
+    left: 0;
+    width: calc(100% + var(--space-xl));
+    transform: translateX(calc(var(--space-xl) * -1));
+    &.active {
+      width: 0%;
+      transition: 300ms ease-in-out transform;
+      right: 0;
+      top: unset;
+      left: unset;
+      transform: unset;
+      transform: translateX(100%);
+    }
+    path {
+      transform: translateX(1px);
+    }
+  }
+  .project-divider-line {
+  }
 }
 </style>
