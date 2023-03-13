31e248d1a486fadee35f3ef02443254d71904dda
rr-machineuser <adam.sebesta5@gmail.com>
2023-03-13 09:19:51 +0100

    add ga4 tracking

--- app.vue
+++ app.vue
@@ -13,6 +13,14 @@ useHead({
       content: "Cloud Native Engineering for the future.",
     },
   ],
+  script: [
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=G-P29FSLM4W9",
+      crossorigin: "anonymous",
+      async: true,
+    },
+    { src: "/ga4.js" },
+  ],
   link: [{ rel: "icon", type: "image/ico", href: "/favicon.ico" }],
 });
 </script>
---
+++ static/ga4.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-P29FSLM4W9");
