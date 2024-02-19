29b2e268edff4c14309e8aaf423e65467b6b455b
Adam Sebesta <hello@adamsebesta.com>
2024-02-19 11:26:42 +0100

    update script

--- app.vue
+++ app.vue
@@ -24,7 +24,7 @@ useHead({
   link: [{ rel: "icon", type: "image/ico", href: "/favicon.ico" }],
 });
 </script>
-<script type="text/javascript">
+<script setup lang="ts"">
   window._mfq = window._mfq || [];
   (function() {
     var mf = document.createElement("script");
